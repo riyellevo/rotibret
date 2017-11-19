6baba4bff750ab90ee0fe5c1187adaaff37af584
riyellevo <vauxhallvectravxr@gmail.com>
2017-11-19 08:06:23 +0700

    login mockup

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -2,6 +2,7 @@ import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams ,AlertController} from 'ionic-angular';
 import {HelloIonicPage} from '../hello-ionic/hello-ionic';
 
+
 /**
  * Generated class for the LoginPage page.
  *
@@ -19,16 +20,24 @@ import {HelloIonicPage} from '../hello-ionic/hello-ionic';
 export class LoginPage {
 
     gotohome(){
-  this.navCtrl.push(HelloIonicPage);
-    
-}
+      this.navCtrl.push(HelloIonicPage);
+
+    }
     @ViewChild('username') uname;
     @ViewChild('password') password;
     
     signIn(){
-    console.log(this.uname.value == 'admin',this.password.value == 'admin');
+    if(this.uname.value == 'admin' && this.password.value == 'admin'){
+         let alert = this.alertCtrl.create({
+          title: 'Login Succ',
+          subTitle: 'Login Succ',
+          buttons: ['OK']
+
+        });
+        alert.present();
+        }
     }
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,public alertCtrl: AlertController) {
   }
 
   ionViewDidLoad() {
